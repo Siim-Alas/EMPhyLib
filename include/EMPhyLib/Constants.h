67f136937dece689fce68f18173c9d730d2f9f6a
Siim Alas <siim.alas@outlook.com>
2021-02-10 16:00:53 +0200

    Defined the physical constants c, epsilon0, and mu0

--- include/EMPhyLib/Constants.h
+++ include/EMPhyLib/Constants.h
@@ -1,1 +1,17 @@
 #pragma once
+
+namespace EMPhyLib
+{
+	/*
+	* The speed of light, 299 792 458 m / s
+	*/
+	static const double c = 2.99792458e+8;
+	/*
+	* Vacuum electric permitivity, 8.8541878128(13) * 10^-12 F / m
+	*/
+	static const double e0 = 8.8541878128131313e-12;
+	/*
+	* Vacuum magnetic permeability 1.25663706212(19) * 10^-6 N * A
+	*/
+	static const double mu0 = 1.2566370621219192e-6;
+}
