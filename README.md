b09ddc418c83ea4d8ddb195549fdfc5984bc9886
Siim Alas <siim.alas@outlook.com>
2021-02-10 13:12:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# EMPhyLib
+
+EMPhyLib is a [C++](https://en.wikipedia.org/wiki/C++) physics library focused 
+on [electromagnetism](https://en.wikipedia.org/wiki/Electromagnetism).
---
+++ src/EMPhyLib.cpp
@@ -0,0 +1,12 @@
+﻿// EMPhyLib.cpp : Defines the entry point for the application.
+//
+
+#include "EMPhyLib.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
